0594ee27d22a514a07fbb028b582f4a78dee7c1a
Jonas Felix de Souza <jonaspetty@hotmail.com>
2021-12-07 22:58:03 -0300

    Atualização do jogo

--- Dino_Run.py
+++ Dino_Run.py
@@ -1,58 +1,97 @@
 import pygame
 from pygame.locals import *
 from sys import exit
+import os
+from random import randrange
+
+diretorio_principal = os.path.dirname(__file__)
+diretorio_imagens = os.path.join(diretorio_principal, 'imagens')
+diretorio_sons = os.path.join(diretorio_principal, 'sons')
 
 pygame.init()
 
 largura = 640
 altura = 480
 
-preto = (0, 0, 0)
+azul = (0, 161, 254)
 
 tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('Sprites')
+pygame.display.set_caption('Dino Game')
 
-class Personagem(pygame.sprite.Sprite):
+sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens, 'sprites.png')).convert_alpha()
+
+class Dino(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.sprites = []
-        self.sprites.append(pygame.image.load('Homem_loko/sprite_0.png'))
-        self.sprites.append(pygame.image.load('Homem_loko/sprite_1.png'))
-        self.sprites.append(pygame.image.load('Homem_loko/sprite_2.png'))
+        self.imagens_jonas = []
+        for i in range(3):
+            img = sprite_sheet.subsurface((i * 32, 0), (32, 32))
+            img = pygame.transform.scale(img, (32*3, 32*3))
+            self.imagens_jonas.append(img)
 
-        self.atual = 0
-        self.image = self.sprites[self.atual]
-        self.image = pygame.transform.scale(self.image, (32*7, 32*7))
-
+        self.index_lista = 0
+        self.image = self.imagens_jonas[self.index_lista]
         self.rect = self.image.get_rect()
-        self.rect.topleft = 300, 253
-
+        self.rect.center = (100, altura - 64)
 
     def update(self):
-        self.atual += 0.4
-        if self.atual >= len(self.sprites):
-            self.atual = 0
-        self.image = self.sprites[int(self.atual)]
-        self.image = pygame.transform.scale(self.image, (32*7, 32*7))
+        if self.index_lista > 2:
+            self.index_lista = 0
+        self.index_lista += 0.25
+        self.image = self.imagens_jonas[int(self.index_lista)]
 
-toda_as_sprites = pygame.sprite.Group()
-personagem = Personagem()
-toda_as_sprites.add(personagem)
+class Nuvens(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = sprite_sheet.subsurface((32*7, 0), (32, 32))
+        self.image = pygame.transform.scale(self.image, (32*3, 32*3))
+        self.rect = self.image.get_rect()
+        self.rect.y = randrange(50, 200, 50)
+        self.rect.x = largura - randrange(30, 300, 90)
 
-imagem_fundo = pygame.image.load('city_background.jpg').convert()
-imagem_fundo = pygame.transform.scale(imagem_fundo, (largura, altura))
+    def update(self):
+        if self.rect.topright[0] < 0:
+            self.rect.x = largura
+            self.rect.y = randrange(50, 200, 50)
+        self.rect.x -= 10
+
+class Chao(pygame.sprite.Sprite):
+    def __init__(self, pos_x):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = sprite_sheet.subsurface((32*6, 0), (32, 32))
+        self.image = pygame.transform.scale(self.image, (32*2, 32*2))
+        self.rect = self.image.get_rect()
+        self.rect.y = altura - 64
+        self.rect.x = pos_x * 64
+
+    def update(self):
+        if self.rect.topright[0] < 0:
+            self.rect.x = largura
+        self.rect.x -= 10
+
+
+todas_as_sprites = pygame.sprite.Group()
+dino = Dino()
+todas_as_sprites.add(dino)
+
+for i in range(4):
+    nuvem = Nuvens()
+    todas_as_sprites.add(nuvem)
+
+for i in range(largura*2//64):
+    chao = Chao(i)
+    todas_as_sprites.add(chao)
 
 relogio = pygame.time.Clock()
-
 while True:
     relogio.tick(30)
-    tela.fill(preto)
+    tela.fill(azul)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
 
-    tela.blit(imagem_fundo, (0, 0))
-    toda_as_sprites.draw(tela)
-    toda_as_sprites.update()
+    todas_as_sprites.draw(tela)
+    todas_as_sprites.update()
+
     pygame.display.flip()
